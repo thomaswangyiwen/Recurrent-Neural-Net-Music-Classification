28bdabd926adc2ea0f2da6e9ef2af471014ebb47
Chase Carthen <chasec2331@gmail.com>
2015-04-07 08:51:20 -0700

    New Dataset Generator created.

--- DatasetGenerator.lua
+++ DatasetGenerator.lua
@@ -22,6 +22,16 @@ classes = {}
 --few of training midis compared to testing or vica-versa
 
 function GatherMidiData(BaseDir) 
+    --Look to see if we have already saved the data.
+    SongData_file = 'SongData.t7'
+    if paths.filep(SongData_file) then
+    	loaded = torch.load(SongData_file)
+	classes = loaded.classes
+	classifier = loaded.classifier
+	SongGroupContainer = loaded.SongGroupContainer
+	return SongGroupContainer
+    end
+
     local SongGroupContainer = {}
     directoryCounter = 0;
     for directoryName in lfs.dir(BaseDir) 
@@ -66,6 +76,15 @@ function GatherMidiData(BaseDir)
             --SerializeData(directoryPath..outputFileName, obj)
         end
     end
+
+    SaveData = 
+    {
+	SongGroupContainer = SongGroupContainer,
+	classes = classes,
+	classifier = classifier
+    }
+    
+    torch.save(SongData_file, SaveData)
     return SongGroupContainer
 end
 
@@ -82,52 +101,32 @@ function SplitMidiData(data, ratio)
     TrainingCounter = 0
     TestingCounter = 0
     for genreKey,value in pairs(data) do 
-        local shuffle = torch.randperm(#data[genreKey].Songs)
-        local numTrain = math.floor(shuffle:size(1) * ratio)
-        local numTest = shuffle:size(1) - numTrain
-            
+        local numTrain = math.floor(#data[genreKey].Songs * ratio)
+        local numTest = #data[genreKey].Songs - numTrain
+	--print(numTrain)
+	--print(numTest)            
+
+
         for i=1,numTrain do
           TrainingCounter = TrainingCounter + 1
           --print(#data[genreKey].Songs)
           --print(i)
           --print(genreKey)
-          trainData.Songs[TrainingCounter] = data[genreKey].Songs[shuffle[i]]:transpose(1,2):clone()
-          trainData.Labels[TrainingCounter] = classifier[genreKey]
+          trainData.Songs[TrainingCounter] = data[genreKey].Songs[i]:transpose(1,2):clone()
+          trainData.Songs[i] = (trainData.Songs[i] - trainData.Songs[i]:mean())/(trainData.Songs[i]:std())
+	  trainData.Labels[TrainingCounter] = classifier[genreKey]
         end
         
         for i=numTrain+1,numTrain+numTest do
             TestingCounter = TestingCounter + 1
-            testData.Songs[TestingCounter] = data[genreKey].Songs[shuffle[i]]:transpose(1,2):clone()
+            testData.Songs[TestingCounter] = data[genreKey].Songs[i]:transpose(1,2):clone()
+            testData.Songs[TestingCounter] = (testData.Songs[TestingCounter] - testData.Songs[TestingCounter]:mean())/(testData.Songs[TestingCounter]:std())
             testData.Labels[TestingCounter] = classifier[genreKey]
-
+	    
         end
     end    
-    
-
-
-
-
-    local shuffledTrainData = {Labels={}, Songs={}}
-    local shuffledTestData = {Labels={}, Songs={}}
-    shuffledTrainData.size = function() return #shuffledTrainData.Songs end
-    shuffledTestData.size = function() return #shuffledTestData.Songs end    
-    --Shuffle all of the data around
-    local shuffle = torch.randperm(TrainingCounter)
-    
-    for i=1, TrainingCounter do
-	shuffledTrainData.Songs[i] = trainData.Songs[shuffle[i]]
-    shuffledTrainData.Songs[i] = (trainData.Songs[shuffle[i]] - trainData.Songs[shuffle[i]]:mean())/(trainData.Songs[shuffle[i]]:std())
-	shuffledTrainData.Labels[i] = trainData.Labels[shuffle[i]]
-    end
-
-    local shuffle = torch.randperm(TestingCounter)
-    for i=1, TestingCounter do
-	shuffledTestData.Songs[i] = testData.Songs[shuffle[i]]
-    shuffledTestData.Songs[i] = (testData.Songs[shuffle[i]] - testData.Songs[shuffle[i]]:mean())/(testData.Songs[shuffle[i]]:std())
-	shuffledTestData.Labels[i] = testData.Labels[shuffle[i]]
-    end
-
-    return shuffledTrainData, shuffledTestData, classes
+   
+    return trainData, testData, classes
 end
 
 
